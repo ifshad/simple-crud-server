d3ae644c84a140b168b2df3b602616ed1f04b7ae
ifshad <shoronshark@gmail.com>
2024-06-16 13:10:16 +0600

    db privacy secured

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://ifshadmte51:1vtu2UBeJhnHKaSx@cluster0.lbhoupe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lbhoupe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
